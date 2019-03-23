9bcdda8f8c90d658bcd9b4ddcb1e36dfdc233e8a
xdustinface <xdustinfacex@gmail.com>
2019-03-23 02:26:46 +0100

    qt: Removed reserve balance from options dialog

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -215,7 +215,6 @@ void OptionsDialog::setMapper()
     mapper->addMapping(ui->unit, OptionsModel::DisplayUnit);
     mapper->addMapping(ui->thirdPartyTxUrls, OptionsModel::ThirdPartyTxUrls);
 
-    mapper->addMapping(ui->reserveBalanceAmount, OptionsModel::ReserveBalance);
 }
 
 void OptionsDialog::setOkButtonState(bool fState)
