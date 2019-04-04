2b5e3a8880ac67c0fb0c2eed32fc7a9593502e69
xdustinface <xdustinfacex@gmail.com>
2019-04-04 20:06:53 +0200

    qt: Confidential typos

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -1105,7 +1105,7 @@ void SendCoinsDialog::coinControlChangeEdited(const QString& text)
         }
         else if (!IsValidDestination(dest)) // Invalid address
         {
-            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Bitcoin Confidentail address"));
+            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Bitcoin Confidential address"));
         }
         else // Valid address
         {
